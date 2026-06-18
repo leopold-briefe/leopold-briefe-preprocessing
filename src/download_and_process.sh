43c6d688323a927e3e9f99eb342a2f37476d726d
csae8092 <p.andorfer@gmail.com>
2026-06-18 13:59:12 +0200

    feat: write dl url

--- src/download_and_process.sh
+++ src/download_and_process.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-export TR_DL_URL=https://transkribus.eu/export/5957408355788940276/export_job_28271258.zip
-
 echo "going to downlaod TEIs from Transkribus from ${TR_DL_URL}"
 wget -O out.zip ${TR_DL_URL}
 rm -rf tmp && mkdir tmp
